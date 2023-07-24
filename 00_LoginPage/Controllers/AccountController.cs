c8ea1ccfabd41f25d10913e86805016ec253287e
ciftci.58@emea.teleperformance.com <ciftci.58@TRISTLT177004>
2023-07-25 00:50:15 +0300

    authentication fixed

--- 00_LoginPage/Controllers/AccountController.cs
+++ 00_LoginPage/Controllers/AccountController.cs
@@ -1,7 +1,8 @@
 ﻿using _00_LoginPage.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 namespace _00_LoginPage.Controllers
 {
@@ -22,42 +23,80 @@ namespace _00_LoginPage.Controllers
         [HttpPost]
         public IActionResult Register(UserModel userModel)
         {
-                if (userModel.Password.Any(x => char.IsUpper(x) && char.IsLower(x) && char.IsDigit(x)))
-                {
-                    
-                }
-                User user = new User(); 
-                user.UserId = userModel.Id;
-                user.FirstName = userModel.FirstName;
-                user.LastName = userModel.LastName;
-                user.Email = userModel.Email;
-                user.Password = userModel.Password;
-                user.CreatedOn = DateTime.Now;
-                userDbContext.Users.Add(user);  
-                userDbContext.SaveChanges();
-                userModel = new UserModel();
-                userModel.SuccessMessage = "Başarılı bir şekilde kayıt oldunuz.";
-                return View("Register");     
+            // 1. Validate User Input
+            //if ( !(userModel.Password.Any(x => char.IsUpper(x) && char.IsLower(x) && char.IsDigit(x)) && userModel.Password.Length < 8))
+            //{
+            //    // Send an error message
+            //    ModelState.AddModelError(key: "Password", errorMessage: "Password should contain an uppercase, lowercase and at least 8 letters.");
+            //    return View(userModel);
+            //}
+
+            // 1. Validate User Input - Passoword should containm 1 uppercase, 1 lowercase, 1 digit and at least 8 letters
+            
+            if (!(userModel.Password.Any(x => char.IsUpper(x)) && userModel.Password.Any(x => char.IsLower(x)) && userModel.Password.Any(x => char.IsDigit(x)) && userModel.Password.Length >= 8))
+            {
+                // Send an error message
+                ModelState.AddModelError(key: "Password", errorMessage: "Password should contain an uppercase, lowercase and at least 8 letters.");
+                return View(userModel);
+            }
+
+            // 2. Create a new User entity
+
+            User user = new User();
+
+            user.UserId = userModel.Id;
+            user.FirstName = userModel.FirstName;
+            user.LastName = userModel.LastName;
+            user.Email = userModel.Email;
+            user.Password = userModel.Password;
+            user.CreatedOn = DateTime.Now;
+
+            // 3. Save this user to the database
+            //userDbContext.Users.Add(user);  
+            //userDbContext.SaveChanges();
+
+            // 4. Redirect user to the "account/login" page
+
+            return RedirectToAction("Login");
         }
 
         public IActionResult Login()
         {
-            LoginModel login = new LoginModel();
-            return View(login);
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Login(LoginModel login)
+        public async Task<IActionResult> Login(LoginModel login)
         {
-            if(userDbContext.Users.Where(user => user.Email == login.Email && user.Password == login.Password).FirstOrDefault() != null)
+            var user = userDbContext.Users.FirstOrDefault(x => x.Email == login.Email && x.Password == login.Password);
+
+            if(user == null)
             {
                 ModelState.AddModelError("Error", "Email veya paralonızı hatalı girdiniz.");
                 return View();
             }
-            else
+
+            var claims = new List<Claim>
             {
-               /* Sess["Email"] = login.Email;*/
-             }
+                // TODO: Add role to the user entity then add user role to the  claim 
+                //new Claim(ClaimTypes.Role, user.),
+                new Claim(ClaimTypes.Name, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var authProperties = new AuthenticationProperties
+            {
+                // Set the expiration time as needed
+                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(30),
+                IsPersistent = true,
+                AllowRefresh = true
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), authProperties);
+
             return View();
         }
     }
--- 00_LoginPage/Program.cs
+++ 00_LoginPage/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
 var app = builder.Build();
 
